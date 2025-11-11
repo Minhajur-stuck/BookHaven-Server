077b43ce8e3d9d47c657fec957eaa0f68176eaa4
Minhajur rahman <minhajurrahman2716@gmail.com>
2025-11-11 21:33:34 +0600

    Added user book collection details route

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = process.env.POST || 3000;
 
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3f7kxdk.mongodb.net/?appName=Cluster0`;
 
@@ -21,73 +21,96 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
-    const db = client.db('bookHaven');
-    const bookCollection = db.collection('allBooks')
-    const userCollection = db.collection('userBooks')
+    const db = client.db("bookHaven");
+    const bookCollection = db.collection("allBooks");
+    const userCollection = db.collection("userBooks");
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
 
-    app.get('/all-books', async(req, res)=>{
+    app.get("/all-books", async (req, res) => {
       const cursor = bookCollection.find();
       const result = await cursor.toArray();
-      res.send(result)
-    })
-
-    
-  
-    app.get('/book-details/:id', async(req, res)=>{
-      const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      const result = await bookCollection.findOne(query);   
-      res.send(result)
-    })
-
-
-    app.get('/myBooks', async(req, res)=>{
-      const cursor = userCollection.find();
+      res.send(result);
+    });
+    app.get("/latest-books", async (req, res) => {
+      const cursor = bookCollection.find().sort({ _id: -1 }).limit(6);
       const result = await cursor.toArray();
-      res.send(result)
-    })
+      res.send(result);
+    });
 
-      app.post('/add-book', async(req, res)=>{
-     const newProduct = req.body;
-     const result = await userCollection.insertOne(newProduct);
-     res.send(result)
-    })
-
-
-    app.patch('/update-book/:id', async(req,res)=>{
+    app.get("/book-details/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id : new ObjectId(id)}
-      const updateBook = req.body
-      const update={
-        $set:{
+      const query = { _id: new ObjectId(id) };
+      const result = await bookCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/userbook-details/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/myBooks", async (req, res) => {
+      const email = req.query.email;
+      
+      const query = {};
+      if (email) {
+        query.userEmail = email;
+      }
+      const cursor = userCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.post("/add-book", async (req, res) => {
+      const newProduct = req.body;
+      const result = await userCollection.insertOne(newProduct);
+      res.send(result);
+    });
+
+    //  const updateInfo = {
+    //   title,
+    //   author,
+    //   genre,
+    //   rating,
+    //   summary,
+    //   image,
+    //   userEmail,
+    //   userName,
+    // };
+
+    app.patch("/update-book/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const updateBook = req.body;
+      console.log(updateBook);
+      const update = {
+        $set: {
           title: updateBook.title,
           author: updateBook.author,
           genre: updateBook.genre,
-          rating:updateBook.rating,
+          rating: updateBook.rating,
           summary: updateBook.summary,
-          coverImage: updateBook.coverImage,
+          coverImage: updateBook.image,
           userEmail: updateBook.userEmail,
-          userName: updateBook.userName
+          userName: updateBook.userName,
+        },
+      };
 
-        }
-      }
+      const result = await userCollection.updateOne(query, update);
+      res.send(result);
+    });
 
-      const result =  await userCollection.updateOne(query, update)
-      res.send(result)
-    })
-
-    app.delete('/delete-book/:id',async(req,res)=>{
+    app.delete("/delete-book/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id : new ObjectId(id)}
-      const result = await userCollection.deleteOne(query)
-      res.send(result)
-    })
-
-
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result);
+    });
   } finally {
     //await client.close();
   }
