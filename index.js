8f87d80c8b54e5aef63434967be9027d6bd2e603
Minhajur rahman <minhajurrahman2716@gmail.com>
2025-11-13 08:13:03 +0600

    Added verifyFirebaseToken middleware to delete-book api

--- index.js
+++ index.js
@@ -160,7 +160,7 @@ async function run() {
       res.send(result);
     });
 
-    app.delete("/delete-book/:id", async (req, res) => {
+    app.delete("/delete-book/:id", verifyFirebaseTOken, async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
       const result = await userCollection.deleteOne(query);
